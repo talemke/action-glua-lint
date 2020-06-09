45a2c2849c2e091917fa9f91ac5f52be3eda20af
Tassilo <tassia710@gmail.com>
2020-06-09 23:30:12 +0200

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ exec.execSync('chmod +x glualint', { cwd: __dirname + '/dependencies' });
 //let result2 = exec.execSync('./glualint ' + process.env.GITHUB_WORKSPACE, { cwd: __dirname + '/dependencies' });
 let result2 = exec.execSync('ls', { cwd: process.env.GITHUB_WORKSPACE });
 
-console.log('Done! Analyzing result...');
+console.log('Done! Analyzing result 2...');
 console.log(result2.toString('utf-8'));
 
 core.setOutput('warnings', 0);
