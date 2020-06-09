d1f427f5a123b382b1d5d4113d11a94022e5d4ea
Tassilo <tassia710@gmail.com>
2020-06-10 00:24:50 +0200

    Update index.js

--- index.js
+++ index.js
@@ -19,13 +19,14 @@ exec.execSync('chmod +x glualint', { cwd: __dirname + '/dependencies' });
 let result2;
 
 try {
+    result2 = exec.execSync('./glualint --version', { cwd: __dirname + '/dependencies' });
     //result2 = exec.execSync('./glualint ' + process.env.GITHUB_WORKSPACE, { cwd: __dirname + '/dependencies' });
-    result2 = exec.execSync('ls', { cwd: __dirname + '/../../../../gmod-cityrp/gmod-cityrp' });
+    //result2 = exec.execSync('ls', { cwd: __dirname + '/../../../../gmod-cityrp/gmod-cityrp' });
 } catch (error) {
     console.log(error.message);
 }
 
-console.log('Done! Analyzing result 15...');
+console.log('Done! Analyzing result 16...');
 console.log(result2.toString('utf-8'));
 
 core.setOutput('warnings', 0);
