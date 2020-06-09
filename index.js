856dd13abcc3dca120442315c8f2a4d382fc4586
Tassilo <tassia710@gmail.com>
2020-06-09 21:57:02 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const exec = require('child_process');
 
 // Fetch NodeJS version (and also test exec functionality)
 let result = exec.execSync('node -v');
-console.log('Running NodeJS ' + result.toString('utf8'));
+console.log('Running NodeJS ' + result.toString('utf-8'));
 
 
 
 // Do the actual linting
 console.log('Linting ' + process.env.GITHUB_WORKSPACE + '...');
-result = exec.execSync('sudo ' + __dirname + '/dependencies/glualint ' + process.env.GITHUB_WORKSPACE);
+let result2 = exec.execSync('sudo ' + __dirname + '/dependencies/glualint ' + process.env.GITHUB_WORKSPACE);
 
 console.log('Done! Analyzing result...');
-console.log(result.toString('utf8'));
+console.log(result2.toString('utf-8'));
 
 core.setOutput('warnings', 0);
 core.setOutput('errors', 0);
