2ada36b09f87598522898d695f024f044f1f2b61
Tassilo <tassia710@gmail.com>
2020-06-09 21:25:23 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const exec = require('child_process');
 try {
     console.log('Linting...');
     exec.execSync('chmod +x ' + __dirname + '/dependencies/glualint');
-    var result = exec.execSync(process.env.GITHUB_WORKSPACE).toString();
+    var result = exec.execSync('sudo ' + __dirname + '/dependencies/glualint ' + process.env.GITHUB_WORKSPACE).toString();
 
     console.log('Done! Analyzing result...');
     console.log(result);
