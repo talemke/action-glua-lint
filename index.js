68a65a3309b567b511187f300e753084f493e9ff
Tassilo <tassia710@gmail.com>
2020-06-09 23:47:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ console.log('Running NodeJS ' + result.toString('utf-8'));
 console.log('Linting ' + process.env.GITHUB_WORKSPACE + '...');
 exec.execSync('chmod +x glualint', { cwd: __dirname + '/dependencies' });
 //let result2 = exec.execSync('./glualint ' + process.env.GITHUB_WORKSPACE, { cwd: __dirname + '/dependencies' });
-let result2 = exec.execSync('ls', { cwd: __dirname + '/../../..' });
+let result2 = exec.execSync('ls', { cwd: __dirname + '/../../../..' });
 
-console.log('Done! Analyzing result 7...');
+console.log('Done! Analyzing result 8...');
 console.log(result2.toString('utf-8'));
 
 core.setOutput('warnings', 0);
