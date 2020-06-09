803ffadbc5fb3173a8cee2281bd4fbee43ba6fe4
Tassilo <tassia710@gmail.com>
2020-06-09 23:56:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -16,9 +16,9 @@ console.log('Running NodeJS ' + result.toString('utf-8'));
 console.log('Linting ' + process.env.GITHUB_WORKSPACE + '...');
 exec.execSync('chmod +x glualint', { cwd: __dirname + '/dependencies' });
 //let result2 = exec.execSync('./glualint ' + process.env.GITHUB_WORKSPACE, { cwd: __dirname + '/dependencies' });
-let result2 = exec.execSync('ls', { cwd: __dirname + '/../../../../gmod-cityrp/TASSIA710/master' });
+let result2 = exec.execSync('ls', { cwd: __dirname + '/../../../../gmod-cityrp' });
 
-console.log('Done! Analyzing result 10...');
+console.log('Done! Analyzing result 11...');
 console.log(result2.toString('utf-8'));
 
 core.setOutput('warnings', 0);
