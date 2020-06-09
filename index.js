d868c86bcd920ff8a24fba7ead76d4c1e23b8a0d
Tassilo <tassia710@gmail.com>
2020-06-09 22:11:18 +0200

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ console.log('Running NodeJS ' + result.toString('utf-8'));
 
 // Do the actual linting
 console.log('Linting ' + process.env.GITHUB_WORKSPACE + '...');
-let result2 = exec.execSync('./glualint --version', { cwd: __dirname + '/dependencies' });
+let result2 = exec.execSync('sudo ./glualint --version', { cwd: __dirname + '/dependencies' });
 
 console.log('Done! Analyzing result...');
 console.log(result2.toString('utf-8'));
