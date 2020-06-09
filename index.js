5911e20c01f36d6d45ffa76cd9c427f8c939f3ea
Tassilo <tassia710@gmail.com>
2020-06-09 20:27:39 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const github = require('@actions/github');
 
 try {
     console.log('Testing GitHub action! Success!');
+
+    core.setOutput('warnings', 0);
+    core.setOutput('errors', 0);
 } catch (error) {
     core.setFailed(error.message);
 }
