941901f34d86cc93c2a1f7874d7eee771009a4a4
Tassilo <tassia710@gmail.com>
2020-06-09 21:51:13 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const github = require('@actions/github');
 // Other modules
 const exec = require('child_process');
 
+// Testing exec
+let result = exec.execSync('node -v');
+console.log(result);
+
 try {
     console.log('Linting ' + process.env.GITHUB_WORKSPACE + '...');
     exec.execSync('chmod +x ' + __dirname + '/dependencies/glualint');
@@ -13,9 +17,6 @@ try {
 
     console.log('Done! Analyzing result...');
     console.log(result);
-
-    let result = exec.spawnSync('ls', []);
-    console.log(result.output.join('\n'));
 
     core.setOutput('warnings', 0);
     core.setOutput('errors', 0);
