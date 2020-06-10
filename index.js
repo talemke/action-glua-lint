4bec935ae4b3864d7d5e911686930487aa3305b7
Tassilo <tassia710@gmail.com>
2020-06-10 20:00:07 +0200

    Changed how the base works

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const github = require('@actions/github');
 // Other modules
 const exec = require('child_process');
 
+// Other constants
+const REGEX = '([a-zA-Z_\\-\\/.]+):\\s\\[([a-zA-Z]+)\\]\\sline\\s([0-9]+),\\scolumn\\s([0-9]+)\\s-\\sline\\s([0-9]+),\\scolumn\\s([0-9]+):\\s+(.*)';
+
 // Fetch NodeJS version (and also test exec functionality)
 let result = exec.execSync('node -v');
 console.log('Running NodeJS ' + result.toString('utf-8').trim());
@@ -25,25 +28,68 @@ try {
     output = error.stdout.toString().trim();
 }
 
+
+
 console.log('Done! Result:');
 console.log('-------------\n');
-console.log(output);
-
-if (!output.includes('[Error]')) return;
 
 let message = '';
-let errors = 0;
-let warnings = 0;
+let errors = {};
+let errorCount = 0;
+let warnings = {};
+let warningCount = 0;
 let elements = output.split('\n');
 
 for (let i = 0; i < elements.length; i++) {
-    if (elements[i].includes('[Error]')) {
-        message += '\n' + elements[i];
-        errors++;
-    } else if (elements[i].includes('[Warning]')) {
-        warnings++;
+    let matches = elements[i].match(REGEX);
+    if (!matches) continue;
+    /* Matches Structure:
+     * [0] = full line
+     * [1] = file path
+     * [2] = type (Error, Warning)
+     * [3] = line (from)
+     * [4] = column (from)
+     * [5] = line (to)
+     * [6] = column (to)
+     * [7] = message
+     */
+    if (matches[2] == 'Error') {
+        errorCount++;
+        if (!errors[matches[7]]) errors[matches[7]] = 1;
+        else errors[matches[7]]++;
+        message += matches[0] + '\n';
+    } else if (matches[2] == 'Warning') {
+        warningCount++;
+        if (!warnings[matches[7]]) warnings[matches[7]] = 1;
+        else warnings[matches[7]]++;
     }
 }
 
-message = 'Found ' + errors + ' error(s) and ' + warnings + ' warning(s):\n' + message;
-core.setFailed(message);
+
+
+if (errorCount != 0) {
+    message = ' Found ' + errorCount + ' error(s) and ' + warningCount + ' warning(s):\n' + message;
+    core.setFailed(message);
+}
+
+
+
+console.log(warningCount + ' warning(s):');
+for (const type in warnings) {
+    console.log(type + ': ' + warnings[type] + 'x');
+}
+console.log('');
+
+
+
+console.log(errorCount + ' error(s):');
+for (const type in errors) {
+    console.log(type + ': ' + errors[type] + 'x');
+}
+console.log('');
+
+
+
+console.log('\n\n------------------------------------------');
+console.log('Full Linter Output (' + elements.length + ' entries):\n\n');
+console.log(output);
