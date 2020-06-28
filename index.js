fd5cf3f69c6986830c45d7fed17d284c81b47521
Tassilo <tassia710@gmail.com>
2020-06-28 16:46:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,9 @@ console.log('Linting ' + process.env.GITHUB_WORKSPACE + core.getInput('directory
 exec.execSync('chmod +x glualint', { cwd: __dirname + '/dependencies' });
 let output;
 try {
-    let result2 = exec.execSync('./glualint --config=' + __dirname + '/dependencies/glualint.json ' + process.env.GITHUB_WORKSPACE + core.getInput('directory'), { cwd: __dirname + '/dependencies' });
+    const cmd = './glualint ' + process.env.GITHUB_WORKSPACE + core.getInput('directory') + ' --config=' + __dirname + '/dependencies/glualint.json';
+    console.log('> ' + cmd);
+    let result2 = exec.execSync(cmd, { cwd: __dirname + '/dependencies' });
     output = result2.stdout.toString().trim();
 } catch (error) {
     output = error.stdout.toString().trim();
